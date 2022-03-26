bb53a4f07183c9a199478632b8395ecb7c6581bb
JustGritt <alexistan9712@gmail.com>
2022-03-26 23:35:06 +0100

    ⚡ Quick fix on mobile navigation

--- src/components/partials/Header.vue
+++ src/components/partials/Header.vue
@@ -35,39 +35,41 @@ header {
         background: rgba(255, 255, 255, .7);
         border-bottom: 10px solid $dark-blue;
     }
-    // @media screen and (max-width: $mobile) { bottom: 0; }
-    nav {
+    @media screen and (max-width: $mobile) { 
+        background: rgba(255, 255, 255, .7); 
+        border-bottom: 10px solid $dark-blue;
+    }
+    nav { 
         position: relative;
-        ul {
-            // @media screen and (max-width: $mobile) { flex-direction: column; }
-            li { 
-                display: flex;
-                margin: 0 25px;
-                text-align: center;
-                a {
-                    font-family: $secondary;
-                    font-size: clamp(14px, 9vw, 24px);
-                    color: $dark-blue;
-                    padding: 10px 15px;
-                    border-radius: 8px;
-                    transition: all .2s ease-in-out;
+        li { 
+            display: flex;
+            margin: 0 25px;
+            text-align: center;
+            a {
+                font-family: $secondary;
+                font-size: clamp(1vw, 9vw, 1.7vw);
+                color: $dark-blue;
+                padding: 10px 15px;
+                border-radius: 8px;
+                transition: all .2s ease-in-out;
+                &:hover { 
+                    background: #373780; 
+                    color: #eee; 
+                }
+                @media screen and (max-width: $mobile) {
+                    font-size: clamp(12px, 9vw, 18px);
+                    margin: 0;
+                    padding: 0;
+                    text-align: center;
+                    
                     &:hover { 
-                        background: #373780; 
-                        color: #eee; 
-                    }
-                    @media screen and (max-width: $mobile) {
-                        font-size: clamp(12px, 9vw, 18px);
-                        margin: 0;
-                        padding: 0;
-                        text-align: center;
-                        &:hover { 
-                            color: #373780; 
-                            background: transparent;
-                        }
+                        color: #373780; 
+                        background: transparent;
                     }
                 }
             }
         }
+        
 
         .dark-mode {
             position: absolute;
